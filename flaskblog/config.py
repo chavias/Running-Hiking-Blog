15899913ed991f534f700ac7edc06299f41abdce
= <matias.chavez@mail.ch>
2024-05-24 09:12:08 +0200

    update init.sql

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = os.getenv('MAIL_PORT')
     MAIL_USERNAME = os.getenv('EMAIL_USER')
--- init.sql
+++ init.sql
@@ -1,1 +1,21 @@
 GRANT ALL PRIVILEGES ON flaskdb.* TO 'flask_user'@'%' IDENTIFIED BY 'flask_password';
+
+USE flaskdb;
+CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    email VARCHAR(120) UNIQUE NOT NULL,
+    image_file VARCHAR(20) NOT NULL DEFAULT 'default.jpg',
+    password VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS post (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    date_posted DATETIME NOT NULL,
+    content TEXT NOT NULL,
+    user_id INT NOT NULL,
+    gpx_file VARCHAR(20),
+    FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
